ff1a3b3e6a9435b9153d9151f86ea10af875dcb0
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-10-19 10:41:15 +0300

    Connected firestore database to flutter project

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
+
+  Future<void> getInfo() async{
+     await Firebase.initializeApp();
+      FirebaseFirestore.instance.collection('chats/hsliijsRc1EdIqDoaxap/messages').snapshots().listen((data) {
+        data.docs.forEach((document) {
+          print(document['text']);
+        });
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +24,12 @@ class ChatScreen extends StatelessWidget {
         child: const Text('Chat Screen'),
       );
     }),
+    floatingActionButton: FloatingActionButton(
+      child: const Icon(Icons.add),
+      onPressed: (){
+       getInfo();
+      },
+    ),
     );
   }
 }
