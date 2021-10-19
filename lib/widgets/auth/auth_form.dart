7c92c1c156bb626d55866ada999b1120f63c7029
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-10-19 12:20:36 +0300

    Added form controls and form save button

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -6,6 +6,22 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final formKey = GlobalKey<FormState>();
+  var isLogin = true;
+  var userEmail = '';
+  var userName = '';
+  var userPassword = '';
+
+  void submit(){
+    final isValid = formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if(isValid){
+      formKey.currentState!.save();
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,23 +31,59 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: const ValueKey('email'),
+                    validator :(value){
+                      if(value!.isEmpty || !value.contains('@')){
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: const InputDecoration(labelText: 'Email Address',),
+                    onSaved: (value){
+                      userEmail = value!;
+                    },
                   ),
+                  if(!isLogin)
                   TextFormField(
+                    key: const ValueKey('username'),
+                    validator :(value){
+                      if(value!.isEmpty || value.length < 4){
+                        return 'Please enter longer username';
+                      }
+                      return null;
+                    },
                     decoration: const InputDecoration(labelText: 'Username'),
+                    onSaved: (value){
+                      userName = value!;
+                    },
                   ),
                   TextFormField(
+                    key: const ValueKey('password'),
+                    validator :(value){
+                      if(value!.isEmpty || value.length < 7){
+                        return 'Password must be at least 7 characters long';
+                      }
+                      return null;
+                    },
                     decoration: const InputDecoration(labelText: 'Password'),
                     obscureText: true,
+                    onSaved: (value){
+                      userPassword = value!;
+                    },
                   ),
                   const SizedBox(height: 10,),
-                  ElevatedButton(child:const Text('Login'),onPressed:(){},),
-                  TextButton(child: const Text('Create new account'),onPressed: (){},),
+                  ElevatedButton(child: Text(isLogin ? 'Login' : 'Sign up'),onPressed:submit,),
+                  TextButton(child: Text(isLogin ? 'Create new account' : 'I already have an account'),onPressed: (){
+                    setState(() {
+                      isLogin = !isLogin;
+                    });
+                  },),
                 ],
             )),
           ),
